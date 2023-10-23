e767c4281433c6e19c6a0b133f53cfbea41e29fa
WanTing <133617159+Wanting1018@users.noreply.github.com>
2023-10-23 11:47:51 +0800

    Add files via upload

---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,327 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txt公分 = new System.Windows.Forms.TextBox();
+            this.txt坪數 = new System.Windows.Forms.TextBox();
+            this.txt公斤 = new System.Windows.Forms.TextBox();
+            this.txt英吋 = new System.Windows.Forms.TextBox();
+            this.txt平方公尺 = new System.Windows.Forms.TextBox();
+            this.txt磅 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.公分轉英吋 = new System.Windows.Forms.Button();
+            this.坪數轉平方公尺 = new System.Windows.Forms.Button();
+            this.公斤轉磅 = new System.Windows.Forms.Button();
+            this.英吋轉公分 = new System.Windows.Forms.Button();
+            this.平方公尺轉坪數 = new System.Windows.Forms.Button();
+            this.磅轉公斤 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label1.Location = new System.Drawing.Point(21, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 37);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "公分互轉英吋";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label2.Location = new System.Drawing.Point(32, 133);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(249, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "坪數互轉平方公尺";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label3.Location = new System.Drawing.Point(32, 262);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(162, 37);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "公斤互轉磅";
+            // 
+            // txt公分
+            // 
+            this.txt公分.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt公分.Location = new System.Drawing.Point(39, 51);
+            this.txt公分.Name = "txt公分";
+            this.txt公分.Size = new System.Drawing.Size(173, 50);
+            this.txt公分.TabIndex = 3;
+            // 
+            // txt坪數
+            // 
+            this.txt坪數.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt坪數.Location = new System.Drawing.Point(39, 173);
+            this.txt坪數.Name = "txt坪數";
+            this.txt坪數.Size = new System.Drawing.Size(173, 50);
+            this.txt坪數.TabIndex = 4;
+            // 
+            // txt公斤
+            // 
+            this.txt公斤.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt公斤.Location = new System.Drawing.Point(39, 308);
+            this.txt公斤.Name = "txt公斤";
+            this.txt公斤.Size = new System.Drawing.Size(173, 50);
+            this.txt公斤.TabIndex = 5;
+            // 
+            // txt英吋
+            // 
+            this.txt英吋.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt英吋.Location = new System.Drawing.Point(439, 57);
+            this.txt英吋.Name = "txt英吋";
+            this.txt英吋.Size = new System.Drawing.Size(173, 50);
+            this.txt英吋.TabIndex = 6;
+            // 
+            // txt平方公尺
+            // 
+            this.txt平方公尺.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt平方公尺.Location = new System.Drawing.Point(439, 173);
+            this.txt平方公尺.Name = "txt平方公尺";
+            this.txt平方公尺.Size = new System.Drawing.Size(173, 50);
+            this.txt平方公尺.TabIndex = 7;
+            // 
+            // txt磅
+            // 
+            this.txt磅.Font = new System.Drawing.Font("微軟正黑體", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txt磅.Location = new System.Drawing.Point(439, 302);
+            this.txt磅.Name = "txt磅";
+            this.txt磅.Size = new System.Drawing.Size(173, 50);
+            this.txt磅.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label4.Location = new System.Drawing.Point(218, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 37);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "公分";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label5.Location = new System.Drawing.Point(628, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 37);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "英吋";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label6.Location = new System.Drawing.Point(218, 179);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(75, 37);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "坪數";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("微軟正黑體", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label7.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label7.Location = new System.Drawing.Point(618, 181);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(96, 26);
+            this.label7.TabIndex = 12;
+            this.label7.Text = "平方公尺";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label8.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label8.Location = new System.Drawing.Point(218, 308);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(75, 37);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "公斤";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("微軟正黑體", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label9.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.label9.Location = new System.Drawing.Point(639, 308);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 37);
+            this.label9.TabIndex = 14;
+            this.label9.Text = "磅";
+            // 
+            // 公分轉英吋
+            // 
+            this.公分轉英吋.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.公分轉英吋.Location = new System.Drawing.Point(313, 39);
+            this.公分轉英吋.Name = "公分轉英吋";
+            this.公分轉英吋.Size = new System.Drawing.Size(75, 37);
+            this.公分轉英吋.TabIndex = 15;
+            this.公分轉英吋.Text = "-->";
+            this.公分轉英吋.UseVisualStyleBackColor = true;
+            this.公分轉英吋.Click += new System.EventHandler(this.公分轉英吋_Click);
+            // 
+            // 坪數轉平方公尺
+            // 
+            this.坪數轉平方公尺.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.坪數轉平方公尺.Location = new System.Drawing.Point(313, 155);
+            this.坪數轉平方公尺.Name = "坪數轉平方公尺";
+            this.坪數轉平方公尺.Size = new System.Drawing.Size(75, 37);
+            this.坪數轉平方公尺.TabIndex = 16;
+            this.坪數轉平方公尺.Text = "-->";
+            this.坪數轉平方公尺.UseVisualStyleBackColor = true;
+            this.坪數轉平方公尺.Click += new System.EventHandler(this.坪數轉平方公尺_Click);
+            // 
+            // 公斤轉磅
+            // 
+            this.公斤轉磅.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.公斤轉磅.Location = new System.Drawing.Point(313, 295);
+            this.公斤轉磅.Name = "公斤轉磅";
+            this.公斤轉磅.Size = new System.Drawing.Size(75, 37);
+            this.公斤轉磅.TabIndex = 17;
+            this.公斤轉磅.Text = "-->";
+            this.公斤轉磅.UseVisualStyleBackColor = true;
+            this.公斤轉磅.Click += new System.EventHandler(this.公斤轉磅_Click);
+            // 
+            // 英吋轉公分
+            // 
+            this.英吋轉公分.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.英吋轉公分.Location = new System.Drawing.Point(313, 82);
+            this.英吋轉公分.Name = "英吋轉公分";
+            this.英吋轉公分.Size = new System.Drawing.Size(75, 37);
+            this.英吋轉公分.TabIndex = 18;
+            this.英吋轉公分.Text = "<--";
+            this.英吋轉公分.UseVisualStyleBackColor = true;
+            this.英吋轉公分.Click += new System.EventHandler(this.英吋轉公分_Click);
+            // 
+            // 平方公尺轉坪數
+            // 
+            this.平方公尺轉坪數.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.平方公尺轉坪數.Location = new System.Drawing.Point(313, 198);
+            this.平方公尺轉坪數.Name = "平方公尺轉坪數";
+            this.平方公尺轉坪數.Size = new System.Drawing.Size(75, 37);
+            this.平方公尺轉坪數.TabIndex = 19;
+            this.平方公尺轉坪數.Text = "<--";
+            this.平方公尺轉坪數.UseVisualStyleBackColor = true;
+            this.平方公尺轉坪數.Click += new System.EventHandler(this.平方公尺轉坪數_Click);
+            // 
+            // 磅轉公斤
+            // 
+            this.磅轉公斤.Font = new System.Drawing.Font("微軟正黑體", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.磅轉公斤.Location = new System.Drawing.Point(313, 338);
+            this.磅轉公斤.Name = "磅轉公斤";
+            this.磅轉公斤.Size = new System.Drawing.Size(75, 37);
+            this.磅轉公斤.TabIndex = 20;
+            this.磅轉公斤.Text = "<--";
+            this.磅轉公斤.UseVisualStyleBackColor = true;
+            this.磅轉公斤.Click += new System.EventHandler(this.磅轉公斤_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(726, 398);
+            this.Controls.Add(this.磅轉公斤);
+            this.Controls.Add(this.平方公尺轉坪數);
+            this.Controls.Add(this.英吋轉公分);
+            this.Controls.Add(this.公斤轉磅);
+            this.Controls.Add(this.坪數轉平方公尺);
+            this.Controls.Add(this.公分轉英吋);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txt磅);
+            this.Controls.Add(this.txt平方公尺);
+            this.Controls.Add(this.txt英吋);
+            this.Controls.Add(this.txt公斤);
+            this.Controls.Add(this.txt坪數);
+            this.Controls.Add(this.txt公分);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "單位轉換器";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txt公分;
+        private System.Windows.Forms.TextBox txt坪數;
+        private System.Windows.Forms.TextBox txt公斤;
+        private System.Windows.Forms.TextBox txt英吋;
+        private System.Windows.Forms.TextBox txt平方公尺;
+        private System.Windows.Forms.TextBox txt磅;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Button 公分轉英吋;
+        private System.Windows.Forms.Button 坪數轉平方公尺;
+        private System.Windows.Forms.Button 公斤轉磅;
+        private System.Windows.Forms.Button 英吋轉公分;
+        private System.Windows.Forms.Button 平方公尺轉坪數;
+        private System.Windows.Forms.Button 磅轉公斤;
+    }
+}
+
---
+++ WindowsFormsApp2/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,205 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        // 顯示黃色的 都是方法(Methods)
+
+        // 從textbox上面獲取的值是字串   -> 因此要轉換
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 公分轉英吋_Click(object sender, EventArgs e)
+        {
+            if (txt公分.Text != "")  // 存放在txt公分.Text 判斷它是否不為空字串
+            {
+                try   // 當try區塊發生錯誤時，就會移向catch區塊，用Exception擷取錯誤
+                      // 把可能產生的例外錯誤的程式碼包起來
+                      // 只能有一個try  
+                      // else那邊不太可能發生錯誤 所以不用包
+                      // 有看到exception 錯誤 可以用try catch
+                {
+                    // 先取得使用者輸入的字串
+                    string strInput = txt公分.Text;  // strInput自行定義的變數
+                                                   // 字串不能拿來運算 所以要轉換成浮點
+                    float myCM = 0.0f;
+                    float myInch = 0.0f;
+                    // 定義一個變數myCM為浮點數 字串轉換後才能放
+
+                    myCM = Convert.ToSingle(strInput);  // 將轉換成的浮點數放在myCM
+                    myInch = myCM * 0.3937f;
+
+                    txt英吋.Text = Convert.ToString(myInch); // 不能指定浮點到字串區 所以要再將浮點轉換成字串
+                    /* 可簡寫成 ：
+                    txt英吋.Text = myInch.ToString();
+                    */
+
+
+                    // 設定值 = 取值
+                }
+                catch(Exception error)  // catch區塊 要跟try同一層級
+                // 例外錯誤就會進來這個catch區塊裡面
+                // try如果沒有產生錯誤 catch區塊就不會執行
+                {
+                    MessageBox.Show(error.Message);
+                    txt英吋.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入公分數值");
+                txt英吋.Text = " ";
+            }
+        }
+
+        // 以下開始自行完成
+
+        private void 英吋轉公分_Click(object sender, EventArgs e)
+        {
+            if (txt英吋.Text != "") 
+            {
+                string strInput = txt英吋.Text;
+                float myCM = 0.0f;
+                float myInch = 0.0f;
+                
+                myInch = Convert.ToSingle(strInput);
+                myCM = myInch * 2.54f;
+
+                txt公分.Text=myCM.ToString();
+                
+
+            }
+            else
+            {
+                MessageBox.Show("請輸入英吋數值");
+                txt公分.Text = "";
+            }
+        }
+
+        private void 坪數轉平方公尺_Click(object sender, EventArgs e)
+        {
+            if (txt坪數.Text != "") 
+            {
+                double my坪數, my平方公尺 = 0.0;  // 同類型的變數可以在同一行定義 用逗號隔開
+                my坪數 = Convert.ToDouble(txt坪數.Text);
+                my平方公尺 = my坪數 * 3.3058;
+                
+                //txt平方公尺.Text = $"{my平方公尺}";
+                //txt平方公尺.Text = my平方公尺.ToString();
+                //txt平方公尺.Text = Convert.ToString(my平方公尺);
+                txt平方公尺.Text = String.Format("{0:F2}", my平方公尺);  // -> 最常用
+                //txt平方公尺.Text = String.Format("{0.F2}", my平方公尺);
+                //// console.writeline 寫的東西可以在string.format()使用
+                //// string.format() -> 字串的格式化
+
+
+            }
+            else
+            {
+                MessageBox.Show("請輸入坪數數值");
+                txt平方公尺.Clear();
+            }
+
+        }
+
+        private void 平方公尺轉坪數_Click(object sender, EventArgs e)
+        {
+            if (txt平方公尺.Text != "")  
+            {
+                double my平方公尺, my坪數 = 0.0;
+                my平方公尺 = Convert.ToDouble(txt平方公尺.Text);
+                my坪數 = my平方公尺 * 0.3025;
+
+                txt坪數.Text = String.Format("{0:F2}", my坪數);  // 0為固定 ; F2到小數點第2位
+            }
+            else
+            {
+                MessageBox.Show("請輸入平方公尺數值");
+                txt坪數.Clear();
+            }
+        }
+
+
+        // 處理例外錯誤(exception error) ex使用者輸入英文 中文 亂碼...等
+        private void 公斤轉磅_Click(object sender, EventArgs e)
+        {
+            if (txt公斤.Text.Length > 0)  
+            {
+                double myKG, myPound = 0.0;
+                bool is轉換成功 = false;
+
+                is轉換成功 = Double.TryParse(txt公斤.Text, out myKG);  // 將字串轉換成double 儲存到myKG
+                // 只是單純的資料轉換
+                // 把第一個字串參數(txt公斤.Text)的資料轉換成double 塞進第二個參數(myKG) 成功的話會回傳true 
+                // 轉換失敗 第二個參數就不動 並且回傳false  (轉換失敗就是當今天輸入者 輸入中文 英文亂碼之類的)
+                // try catch 能處理所有的例外錯誤 (陣列操作 集合操作這些的錯誤)
+                // tryparse 只能處理資料型態轉換的例外錯誤
+                // out 可以視為ref 可以為空 ref不行為空
+
+
+                if (is轉換成功 == true) 
+                {
+                    // 轉換成功
+                    myPound = myKG * 2.2;
+                    txt磅.Text = String.Format("{0:F2}", myPound);
+                }
+                else
+                {
+                    // 轉換失敗
+                    MessageBox.Show("輸入公斤數值有錯誤");
+                    txt磅.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入公斤數值");
+                txt磅.Clear();             
+            }
+        }
+
+        private void 磅轉公斤_Click(object sender, EventArgs e)
+        {
+            if (txt磅.Text.Length > 0) 
+            {
+                double my磅, my公斤 = 0.0;
+                bool is轉換成功 = false;
+
+                is轉換成功 = Double.TryParse(txt磅.Text, out my磅);
+                
+                    
+                if(is轉換成功 == true)
+                {
+                    my公斤 = my磅 * 0.453;
+                    txt公斤.Text = String.Format("{0:F2}", my公斤);
+                }
+                else
+                {
+                    MessageBox.Show("輸入的磅數有錯誤");
+                    txt公斤.Clear();
+                }
+            }
+            else
+            {
+                MessageBox.Show("請輸入磅數");
+                txt公斤.Clear();
+            }
+        }
+    }
+}
